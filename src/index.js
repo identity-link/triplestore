9a816022507482aee45335bb9e26fcde8b38c304
Zach Smith <zachsmith.sa@gmail.com>
2020-03-08 15:03:15 +0200

    Initial test with N3.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-console.log(process.env.NODE_ENV)
-setInterval(() => console.log('hi'), 1000)
+// https://github.com/rdfjs/N3.js#storing
+import { DataFactory, Writer } from 'n3'
+
+const { namedNode, literal, defaultGraph, quad } = DataFactory
+const myQuad = quad(
+  namedNode('https://ruben.verborgh.org/profile/#me'),
+  namedNode('http://xmlns.com/foaf/0.1/givenName'),
+  literal('Ruben', 'en'),
+  defaultGraph()
+)
+console.log(myQuad.subject.value) // https://ruben.verborgh.org/profile/#me
+console.log(myQuad.object.value) // Ruben
+console.log(myQuad.object.datatype.value) // http://www.w3.org/1999/02/22-rdf-syntax-ns#langString
+console.log(myQuad.object.language) // en
+
+const writer = new Writer({ prefixes: { c: 'http://example.org/cartoons#' } })
+writer.addQuad(
+  namedNode('http://example.org/cartoons#Tom'),
+  namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+  namedNode('http://example.org/cartoons#Cat')
+)
+writer.addQuad(
+  quad(
+    namedNode('http://example.org/cartoons#Tom'),
+    namedNode('http://example.org/cartoons#name'),
+    literal('Tom')
+  )
+)
+
+writer.end((error, result) => console.log(result))
